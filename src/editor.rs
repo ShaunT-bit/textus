2308b1927875a30f784b73a92b194b9a245a01c2
ShaunT-bit <tshaun114@gmail.com>
2025-06-22 12:03:47 +0400

    Added a title

--- src/editor.rs
+++ src/editor.rs
@@ -1,7 +1,14 @@
-use crossterm::event::{read,Event,Event::Key,KeyCode::Char,KeyEvent,KeyModifiers};
+
+use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
 use std::io::Error;
+
+
 mod terminal;
 use terminal::{Terminal,Size,Position};
+
+
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub struct Editor {
     should_quit: bool,
@@ -55,7 +62,7 @@ impl Editor {
             
         } else {
             Self::draw_rows()?;
-            Terminal::move_cursor_to(Position{x:0, y:0})?;
+            Terminal::move_cursor_to(Position {x: 0, y:0})?;
             
         }
         
@@ -64,12 +71,34 @@ impl Editor {
         Ok(())
     }
     
+    fn draw_welcome_message() -> Result<(), Error> {
+        let mut welcome_message = format!("{NAME} editor --version {VERSION}");
+        
+        let width = Terminal::size()?.width as usize;
+        let len = welcome_message.len();
+        let padding = (width-len)/2;
+        let spaces = " ".repeat(padding-1);
+        welcome_message = format!("~{spaces}{welcome_message}");
+        welcome_message.truncate(width);
+        Terminal::print(welcome_message)?;
+        Ok(())
+    }
+    
+    fn draw_empty_row() -> Result<(), Error> {
+        Terminal::print("~")?;
+        Ok(())
+    }
+
     fn draw_rows() -> Result<(), Error> {
-        let Size{height, ..} = Terminal::size()?;
+        let Size { height, .. } = Terminal::size()?;
         for current_row in 0..height {
-            Terminal::clear_screen()?;
-            Terminal::print("~")?;
-            if current_row+1 < height {
+            Terminal::clear_line()?;
+            if current_row == height / 3 {
+                Self::draw_welcome_message()?;
+            } else {
+                Self::draw_empty_row()?;
+            }
+            if current_row + 1 < height {
                 Terminal::print("\r\n")?;
             }
         }
