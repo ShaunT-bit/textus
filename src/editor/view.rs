3c500f3d2b5bd32a2364c981e3cf075422bb2a0c
ShaunT-bit <tshaun114@gmail.com>
2025-06-22 19:26:50 +0400

    U can now open files

--- src/editor/view.rs
+++ src/editor/view.rs
@@ -1,32 +1,24 @@
-use super::terminal::{Size, Terminal};
+use super::terminal::{Size,Terminal};
 use std::io::Error;
-
-
 
 mod buffer;
 use buffer::Buffer;
 
-const NAME: & str = env!("CARGO_PKG_NAME");
-const VERSION: & str = env!("CARGO_PKG_VERSION");
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 
 #[derive(Default)]
 pub struct View {
     buffer: Buffer,
 }
 
-
 impl View {
-    pub fn render(&self) ->Result<(), Error> {
+    pub fn render_welcome_screen()  -> Result<(), Error> {
+        let Size {height, ..} = Terminal::size()?;
         
-        let Size {height, .. } = Terminal::size()?;
         for current_row in 0..height {
             Terminal::clear_line()?;
-            
-            if let Some(line) = self.buffer.lines.get(current_row) {
-                Terminal::print(line)?;
-                Terminal::print("\r\n")?;
-                continue;
-            }
             
             #[allow(clippy::integer_division)]
             if current_row == height/3 {
@@ -35,18 +27,42 @@ impl View {
                 Self::draw_empty_row()?;
             }
             
-            if current_row.saturating_add(1) < height {
+            if current_row.saturating_add(1) <height {
                 Terminal::print("\r\n")?;
-                
             }
         }
         Ok(())
     }
     
-    fn draw_welcome_message() ->Result<(), Error> {
+    pub fn render_buffer(&self) -> Result<(), Error> {
+        let Size {height, ..} = Terminal::size()?;
         
+        for current_row in 0..height {
+            Terminal::clear_line()?;
+            
+            if let Some(line) = self.buffer.lines.get(current_row) {
+                Terminal::print(line)?;
+                Terminal::print("\r\n")?;
+            } else {
+                Self::draw_empty_row()?;
+            }
+            
+            
+        }
+        Ok(())
+    }
+    
+    pub fn render(&self) -> Result<(), Error> {
+        if self.buffer.is_empty() {
+            Self::render_welcome_screen()?;
+        } else {
+            self.render_buffer()?;
+        }
+        Ok(())
+    }
+    
+    fn draw_welcome_message() -> Result<(), Error> {
         let mut welcome_message = format!("{NAME} Editor -- Version {VERSION}");
-        
         let width = Terminal::size()?.width;
         let len = welcome_message.len();
         
@@ -55,14 +71,23 @@ impl View {
         let padding = (width.saturating_sub(len)) / 2;
         
         let spaces = " ".repeat(padding.saturating_sub(1));
+        
         welcome_message = format!("~{spaces}{welcome_message}");
+        welcome_message.truncate(width);
+        
         Terminal::print(&welcome_message)?;
         
         Ok(())
     }
     
-    fn draw_empty_row() ->Result<(), Error> {
+    fn draw_empty_row() -> Result<(), Error> {
         Terminal::print("~")?;
         Ok(())
+    }
+    
+    pub fn load(&mut self, file_name: &str){
+        if let Ok(buffer) = Buffer::load(file_name) {
+            self.buffer = buffer;
+        }
     }
 }
