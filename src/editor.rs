0198aeb34ee3a7cc565527079a75694754c73e32
ShaunT-bit <tshaun114@gmail.com>
2025-06-22 20:52:38 +0400

    Improved Rendering

--- src/editor.rs
+++ src/editor.rs
@@ -1,23 +1,17 @@
-use std::{io::Error,env};
 use core::cmp::min;
-
-use crossterm::event::{read, Event::{self, Key}, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
-
-
-
-
-
+use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use std::{env, io::Error};
 mod terminal;
 mod view;
-
 use terminal::{Position, Size, Terminal};
 use view::View;
 
-#[derive(Copy,Clone,Default)]
+#[derive(Copy, Clone, Default)]
 struct Location {
     x: usize,
     y: usize,
 }
+
 #[derive(Default)]
 pub struct Editor {
     should_quit: bool,
@@ -26,7 +20,6 @@ pub struct Editor {
 }
 
 impl Editor {
-    
     pub fn run(&mut self) {
         Terminal::initialize().unwrap();
         self.handle_args();
@@ -34,33 +27,27 @@ impl Editor {
         Terminal::terminate().unwrap();
         result.unwrap();
     }
-    
     fn handle_args(&mut self) {
         let args: Vec<String> = env::args().collect();
         if let Some(file_name) = args.get(1) {
             self.view.load(file_name);
-        } 
+        }
     }
-    
+
     fn repl(&mut self) -> Result<(), Error> {
         loop {
             self.refresh_screen()?;
-            
             if self.should_quit {
                 break;
             }
-            
             let event = read()?;
-            self.evaluate_event(&event)?;
+            self.evaluate_event(event)?;
         }
         Ok(())
     }
-    
-    fn move_point(&mut self, key_code:KeyCode) -> Result<(),Error> {
-        
-        let Location {mut x,mut y} = self.location;
-        let Size {height, width} = Terminal::size()?;
-        
+    fn move_point(&mut self, key_code: KeyCode) -> Result<(), Error> {
+        let Location { mut x, mut y } = self.location;
+        let Size { height, width } = Terminal::size()?;
         match key_code {
             KeyCode::Up => {
                 y = y.saturating_sub(1);
@@ -91,57 +78,69 @@ impl Editor {
         self.location = Location { x, y };
         Ok(())
     }
-    
-    fn evaluate_event(&mut self, event: &Event) -> Result<(),Error> {
-        if let Key(KeyEvent {
-            code,
-            modifiers,
-            kind: KeyEventKind::Press,
-            ..
-                   }) = event {
-            match code {
-                
-                KeyCode::Char('q') if *modifiers == KeyModifiers::CONTROL => {
+
+    // needless_pass_by_value: Event is not huge, so there is not a
+    // performance overhead in passing by value, and pattern matching in this
+    // function would be needlessly complicated if we pass by reference here.
+    #[allow(clippy::needless_pass_by_value)]
+    fn evaluate_event(&mut self, event: Event) -> Result<(), Error> {
+        match event {
+            Event::Key(KeyEvent {
+                           code,
+                           kind: KeyEventKind::Press,
+                           modifiers,
+                           ..
+                       }) => match (code, modifiers) {
+                (KeyCode::Char('q'), KeyModifiers::CONTROL) => {
                     self.should_quit = true;
                 }
-                
-                KeyCode::Up
-                |KeyCode::Down
-                |KeyCode::Left
-                |KeyCode::Right
-                |KeyCode::PageDown
-                |KeyCode::PageUp
-                |KeyCode::End
-                |KeyCode::Home => {
-                    self.move_point(*code)?;
+                (
+                    KeyCode::Up
+                    | KeyCode::Down
+                    | KeyCode::Left
+                    | KeyCode::Right
+                    | KeyCode::PageDown
+                    | KeyCode::PageUp
+                    | KeyCode::End
+                    | KeyCode::Home,
+                    _,
+                ) => {
+                    self.move_point(code)?;
                 }
-                _ => (),
+                _ => {}
+            },
+            Event::Resize(width_u16, height_u16) => {
+                // clippy::as_conversions: Will run into problems for rare edge case systems where usize < u16
+                #[allow(clippy::as_conversions)]
+                let height = height_u16 as usize;
+                // clippy::as_conversions: Will run into problems for rare edge case systems where usize < u16
+                #[allow(clippy::as_conversions)]
+                let width = width_u16 as usize;
+                self.view.resize(Size {
+                    height,
+                    width,
+                });
             }
+            _ => {}
         }
         Ok(())
-        
     }
-    
     fn refresh_screen(&mut self) -> Result<(), Error> {
         Terminal::hide_caret()?;
         Terminal::move_caret_to(Position::default())?;
-        
         if self.should_quit {
             Terminal::clear_screen()?;
-            Terminal::print("GoodBye.\r\n")?;
+            Terminal::print("Goodbye.\r\n")?;
         } else {
             self.view.render()?;
             Terminal::move_caret_to(Position {
                 col: self.location.x,
                 row: self.location.y,
             })?;
-            
         }
-        
+
         Terminal::show_caret()?;
         Terminal::execute()?;
         Ok(())
-        
     }
-    
 }
