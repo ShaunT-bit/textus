aa460e86893c234a2d3248c5205666710eab62d6
ShaunT-bit <tshaun114@gmail.com>
2025-06-24 09:32:50 +0400

    BIG UPDAte: added grapheme libary

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,10 @@
     clippy::arithmetic_side_effects,
     clippy::as_conversions,
     clippy::integer_division
-)] //this has to be later removed while shipping
-
-
+)]
 mod editor;
 use editor::Editor;
-
-
 
 fn main() {
     Editor::new().unwrap().run();
 }
-
